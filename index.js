29f3a7c7fe9d74436796130ca3be1e759caec7e0
John <nadenjohn@gmail.com>
2022-02-25 18:00:18 -0800

    commit tree 7ccc250c0d851b7bb3de5ac7ebd815372ee07395

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// // for (let age = 30; age < 40; age++) {
+// //     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// // }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i <gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+const cards = [];
+function writeCards (arr, event) {
+    for (let i = 0; i < arr.length; i++) {
+        cards.push (`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+// function wrapGifts(gifts) {
+//     let i = 0;
+//     while (i < gifts.length) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         i++
+//     }
+//     return gifts
+// }
+// wrapGifts(gifts);
+
+function countDown (num){
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
+ 
